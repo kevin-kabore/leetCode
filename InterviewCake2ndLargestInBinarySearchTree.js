63b11689f747cc28fedab2d4f402d9d6702d7d54
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-20 15:25:05 -0700

    completes ex

---
+++ InterviewCake2ndLargestInBinarySearchTree.js
@@ -0,0 +1,51 @@
+function BinaryTreeNode(value) {
+  this.value = value;
+  this.left  = null;
+  this.right = null;
+}
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+  this.left = new BinaryTreeNode(value);
+  return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+  this.right = new BinaryTreeNode(value);
+  return this.right;
+};
+
+
+function findLargest(rootNode) {
+  var current = rootNode;
+  while (current) {
+      if (!current.right) return current.value;
+      current = current.right;
+  }
+}
+
+function findSecondLargest(rootNode) {
+  if (!rootNode || (!rootNode.left && !rootNode.right)) {
+      throw new Error('Tree must have at least 2 nodes');
+  }
+
+  var current = rootNode;
+
+  while (current) {
+
+      // case: current is largest and has a left subtree
+      // 2nd largest is the largest in that subtree
+      if (current.left && !current.right) {
+          return findLargest(current.left);
+      }
+
+      // case: current is parent of largest, and largest has no children,
+      // so current is 2nd largest
+      if (current.right &&
+              !current.right.left &&
+              !current.right.right) {
+          return current.value;
+      }
+
+      current = current.right;
+  }
+}
